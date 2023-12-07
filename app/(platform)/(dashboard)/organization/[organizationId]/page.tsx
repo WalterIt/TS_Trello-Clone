a8b2a3090819322b28afcb5a1481f2b429c164bb
WalterIt <whiruba@gmail.com>
2023-12-07 02:00:08 -0300

    01:39:54 Enabled Organizations in Clerk Site. Created Organizational Folders: Dashbord-Layout, Navbar Component. Implemented Middleware.ts

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,11 @@
+
+const OrganizationIdPage = () => {
+
+    return ( 
+        <div className="h-screen">
+            <h1>Organization  Page</h1>
+        </div>
+     );
+}
+ 
+export default OrganizationIdPage;

75c59a550be803538fa5f2bd95081146d3787f6b
WalterIt <whiruba@gmail.com>
2023-12-11 09:13:12 -0300

    04:18:00 Form Components

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { createBoard } from "@/actions/create-board";
-import { FormInput } from "./input";
-import { FormButton } from "./form-button";
 import { useAction } from "@/hooks/use-action";
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
 
 export const Form = () => {
     const { execute, fieldErrors } = useAction(createBoard, {
@@ -18,16 +18,23 @@ export const Form = () => {
     const onSubmit = (formData: FormData) => {
         const title = formData.get("title") as string;
 
+        console.log({title});
+
         execute({ title });
     }
 
     return (
         <form action={onSubmit} >
-            <div className="flex flex-col space-y-2">
-                
-                <FormInput errors={fieldErrors} />
+            <div className="flex flex-col space-y-2">                
+                <FormInput 
+                    label="Board Title"
+                    id="title" 
+                    errors={fieldErrors} 
+                />
             </div>
-            <FormButton />
+            <FormSubmit>
+                Save
+            </FormSubmit>
         </form>
     )
 }
---
+++ components/form/form-submit.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+
+interface FormSubmitProps {
+    children: React.ReactNode;
+    disabled?: boolean;
+    className?: string;
+    variant?: "default" | "primary" | "destructive" | "outline" | "ghost" | "link" | "secondary";
+}
+
+export const FormSubmit = ({ children, disabled, className, variant }: FormSubmitProps) => {
+    const { pending } = useFormStatus();
+
+    return (
+        <Button
+           disabled={disabled || pending}
+           type="submit"
+           variant={variant}
+           size="sm"
+           className={cn(className)}
+        >
+            {children}
+        </Button>
+    )
+}

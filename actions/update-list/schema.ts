eab0d8830dd34509015891645c965c862f59fdc7
WalterIt <whiruba@gmail.com>
2023-12-14 11:19:43 -0300

    07:18:11 List Header: Render List Array -Created ListItem Component. 07:26:21 Implemented Rename List Functionality & Server Action

---
+++ actions/update-list/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const UpdateList = z.object({
+    title: z.string({
+        required_error: "Title is required",
+        invalid_type_error: "Title is required",
+    }).min(2, {message:"Title must be at least 3 characters"}),
+    id: z.string(),
+    boardId: z.string()
+})

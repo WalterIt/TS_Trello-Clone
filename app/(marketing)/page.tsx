71b0c83de1f4bcae38027c2821ea0309c94222b9
WalterIt <whiruba@gmail.com>
2023-12-19 11:41:17 -0300

    Fixed: "headingFont" is not a valid Page export field

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ import LocalFont from "next/font/local";
 import {Poppins} from "next/font/google";
 import { cn } from "@/lib/utils";
 
-export const headingFont = LocalFont({
+const headingFont = LocalFont({
     src: "../../public/fonts/font.woff2",
 })
 
